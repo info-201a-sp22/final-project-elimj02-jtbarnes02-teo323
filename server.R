ef3abc43f7998bc0aee91e17374a7ae4361d2f07
Teo323 <teo.k@inkpool.net>
2022-06-01 19:24:23 -0700

    read.csv

--- server.R
+++ server.R
@@ -4,4 +4,4 @@ library(ggplot2)
 library(dplyr)
 library("tidyverse")
 
-student_drinking_df <- read.csv("https://data.world/data-society/student-alcohol-consumption")
+student_drinking_df <- read.csv("~/Documents/INFO201Code/final-project-elimj02/student_drinking_df.csv")
--- ui.R
+++ ui.R
@@ -5,4 +5,4 @@ library(dplyr)
 library("tidyverse")
 library(bslib)
 
-student_drinking_df <- read.csv("https://data.world/data-society/student-alcohol-consumption")
+student_drinking_df <- read.csv("~/Documents/INFO201Code/final-project-elimj02/student_drinking_df.csv")
