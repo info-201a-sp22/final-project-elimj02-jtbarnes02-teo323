7fe68149075455383c85e627dced65594ede81a0
Teo323 <teo.k@inkpool.net>
2022-06-01 22:10:59 -0700

    teo parents drink graph and widget

--- server.R
+++ server.R
@@ -8,6 +8,32 @@ student_drinking_df <- read.csv("https://query.data.world/s/r47f3cqixzyczsmnwqbr
 student_drinking_df <- student_drinking_df %>% 
   mutate(Talc = Walc + Dalc)
 
+#Visualization 1 data table
+fam_relation_df <- student_drinking_df %>%
+  select(sex, age, Pstatus, famrel, Talc)
+
+fam_relation_df$Pstatus <- gsub("T", "Together", as.character(fam_relation_df$Pstatus))
+fam_relation_df$Pstatus <- gsub("A", "Apart", as.character(fam_relation_df$Pstatus))
+
+parents_status <- fam_relation_df %>%
+  group_by(Pstatus, famrel) %>%
+  summarize(alc_avg = mean(Talc))
+
 server <- function(input, output) {
+  
+  output$fam_relation_plot <- renderPlotly({
+    
+    fam_relation_df <- parents_status %>%
+      filter(famrel %in% input$scale_select)
+    
+    fam_relation_plot <- ggplot(data = fam_relation_df) + 
+      geom_bar(mapping = aes(x = Pstatus, y = alc_avg, fill = Pstatus), stat='identity') +
+      ylim(0, 10) +
+      labs(title = "How Family Relations Impact Drinking",
+           x = "Parent Marital Status",
+           y = "Weekly Alcohol Consumption Scale (1-10)")
+    
+    return(fam_relation_plot)
+  })
   
 }
--- ui.R
+++ ui.R
@@ -9,6 +9,17 @@ student_drinking_df <- read.csv("https://query.data.world/s/r47f3cqixzyczsmnwqbr
 student_drinking_df <- student_drinking_df %>% 
   mutate(Talc = Walc + Dalc)
 
+#Visualization 1 data table
+fam_relation_df <- student_drinking_df %>%
+  select(sex, age, Pstatus, famrel, Talc)
+
+fam_relation_df$Pstatus <- gsub("T", "Together", as.character(fam_relation_df$Pstatus))
+fam_relation_df$Pstatus <- gsub("A", "Apart", as.character(fam_relation_df$Pstatus))
+
+parents_status <- fam_relation_df %>%
+  group_by(Pstatus, famrel) %>%
+  summarize(alc_avg = mean(Talc))
+
 
 intro_tab <- tabPanel(
   'Introduction',
@@ -17,8 +28,22 @@ intro_tab <- tabPanel(
   )
 )
 
+scale_widget <- selectInput(
+  inputId = "scale_select",
+  label = "Family Relationship Scale",
+  choices = parents_status$famrel,
+  multiple = F,
+)
+
+main_panel_plot <- mainPanel(
+  plotlyOutput(outputId = "fam_relation_plot")
+)
+
 viz_1_tab <- tabPanel(
   'Visualization 1',
+  sidebarLayout(
+    sidebarPanel(scale_widget),
+    main_panel_plot),
   fluidPage(
     p('Description')
   )
